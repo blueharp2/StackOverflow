932ed35188ee3974f8e0ee220c9a7a08746ec625
LINDSEY BOGGIO <BLUEHARP2@GMAIL.COM>
2015-12-07 15:27:51 -0800

    Burger Menu progress continued

--- MenuContainerViewController.m
+++ MenuContainerViewController.m
@@ -38,6 +38,8 @@ NSTimeInterval const kburgerTimeToSlideMenuOpen = 0.2;
 - (void)viewDidLoad {
     [super viewDidLoad];
     [self setupAllViewControllers];
+    [self setupBurgerButton];
+    [self setupPanGesture];
 }
 
 -(void) setupAllViewControllers{
@@ -73,6 +75,69 @@ NSTimeInterval const kburgerTimeToSlideMenuOpen = 0.2;
     self.myQuestionsViewController = myQuestionsVC;
 }
 
+-(void) setupBurgerButton {
+    UIButton *burgerButton = [[UIButton alloc]initWithFrame:CGRectMake(0.0, 0.0, kburgerButtonWidth, kburgerButtonHeight)];
+    [burgerButton setImage:[UIImage imageNamed:@"hamburger"] forState:UIControlStateNormal];
+    [self.topViewController.view addSubview:burgerButton];
+    [burgerButton addTarget:self action:@selector(burgerButtonPressed:) forControlEvents:UIControlEventTouchUpInside];
+    self.burgerButton = burgerButton;
+}
+
+-(void) burgerButtonPressed: (UIButton *)sender {
+    [UIView animateWithDuration:kburgerTimeToSlideMenuOpen animations:^{
+        self.topViewController.view.center = CGPointMake(self.view.center.x *kburgerOpenScreenMultiplier, self.topViewController.view.center.y);
+    } completion:^(BOOL finished) {
+        UITapGestureRecognizer *tap = [[UITapGestureRecognizer alloc]initWithTarget:self action:@selector(tapToCloseMenu:)];
+        [self.topViewController.view addGestureRecognizer:tap];
+        sender.userInteractionEnabled = false;
+
+    }];
+}
+
+-(void) tapToCloseMenu:(UITapGestureRecognizer *)tap{
+    [self.topViewController.view removeGestureRecognizer:tap];
+    [UIView animateWithDuration:kburgerTimeToSlideMenuOpen animations:^{
+        self.topViewController.view.center = self.view.center;
+    }completion:^(BOOL finished) {
+        self.burgerButton.userInteractionEnabled = YES;
+    
+    }];
+}
+
+-(void) setupPanGesture{
+    UIPanGestureRecognizer *pan = [[UIPanGestureRecognizer alloc]initWithTarget:self action:@selector(topViewControllerPanned:)];
+    [self.topViewController.view addGestureRecognizer:pan];
+    self.panGesture = pan;
+}
+
+-(void) topViewControllerPanned:(UIPanGestureRecognizer *) sender{
+    CGPoint velocity = [sender velocityInView:self.topViewController.view];
+    CGPoint translation = [sender translationInView:self.topViewController.view];
+    
+    if (sender.state == UIGestureRecognizerStateChanged){
+        if (velocity.x > 0){
+            CGPointMake(self.topViewController.view.center.x + translation.x, self.topViewController.view.center.y);
+            [sender setTranslation:CGPointZero inView:self.topViewController];
+        }
+    }
+    if (sender.state == UIGestureRecognizerStateEnded){
+        if (self.topViewController.view.frame.origin.x > self.topViewController.view.frame.size.width /kburgerOpenScreenDivider){
+            [UIView animateWithDuration:kburgerTimeToSlideMenuOpen animations:^{
+                self.topViewController.view.center = CGPointMake(self.view.center.x * kburgerOpenScreenMultiplier, self.topViewController.view.center.y);
+            } completion:^(BOOL finished) {
+                UITapGestureRecognizer *tap = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(tapToCloseMenu:)];
+                [self.topViewController.view addGestureRecognizer:tap];
+                self.burgerButton.userInteractionEnabled = NO;
+            }];
+        }else {
+            [UIView animateWithDuration:kburgerTimeToSlideMenuOpen animations:^{
+                self.topViewController.view.center = CGPointMake(self.view.center.x, self.topViewController.view.center.y);
+            }completion:^(BOOL finished) {
+                
+            }];
+        }
+    }
+}
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
