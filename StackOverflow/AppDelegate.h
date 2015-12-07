9f6691e4f0c6947c2f86bd4c05035bbf5b90c526
LINDSEY BOGGIO <BLUEHARP2@GMAIL.COM>
2015-12-07 12:47:27 -0800

    Initial Commit

---
+++ StackOverflow/AppDelegate.h
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.h
+//  StackOverflow
+//
+//  Created by Lindsey on 12/7/15.
+//  Copyright © 2015 Lindsey Boggio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+
+@end
+
