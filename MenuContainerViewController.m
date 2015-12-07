2cba245c4643200a15bb48a24ee4ea167b5bacbc
LINDSEY BOGGIO <BLUEHARP2@GMAIL.COM>
2015-12-07 14:09:43 -0800

    start of Burger Menu

--- MenuContainerViewController.m
+++ MenuContainerViewController.m
@@ -7,8 +7,29 @@
 //
 
 #import "MenuContainerViewController.h"
+#import "MenuTableViewController.h"
+#import "MainContentViewController.h"
+#import "MyQuestionsViewController.h"
 
-@interface MenuContainerViewController ()
+CGFloat const kburgerOpenScreenDivider = 3.0;
+CGFloat const kburgerOpenScreenMultiplier = 2.0;
+CGFloat const kburgerButtonWidth = 50.0;
+CGFloat const kburgerButtonHeight = 50.0;
+
+NSTimeInterval const kburgerTimeToSlideMenuOpen = 0.2;
+
+
+
+@interface MenuContainerViewController () <UITableViewDelegate>
+
+@property (strong, nonatomic) MenuTableViewController *leftMenuViewController;
+@property (strong, nonatomic) MainContentViewController *topViewController;
+@property (strong, nonatomic) MyQuestionsViewController *myQuestionsViewController;
+
+@property (strong, nonatomic) UIButton *burgerButton;
+@property (strong, nonatomic) UIPanGestureRecognizer *panGesture;
+
+@property (strong, nonatomic) NSArray *viewControllers;
 
 @end
 
