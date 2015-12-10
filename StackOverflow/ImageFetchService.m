0f216de43603c8b949196460a371b1de5e58402d
LINDSEY BOGGIO <BLUEHARP2@GMAIL.COM>
2015-12-09 16:00:38 -0800

    have created a new image fetcher func for waterfall

--- StackOverflow/ImageFetchService.m
+++ StackOverflow/ImageFetchService.m
@@ -11,9 +11,39 @@
 
 @implementation ImageFetchService
 
++(void) lulTest123 {
+}
+
+
++(void) fetchImagesWithDispatchQueue:(dispatch_queue_t)imgQueue withUrl:(NSURL * )url completionHandler:(kNSImageCompletionHandler)completionHandler {
+    
+    dispatch_async(imgQueue, ^{
+        NSError *error;
+        NSData *data = [NSData dataWithContentsOfURL:url options:NSDataReadingUncached error:&error];
+        
+        UIImage *results = [[UIImage alloc]initWithData:data];
+        dispatch_async(dispatch_get_main_queue(), ^{
+            if (error == nil && data !=nil) {
+                completionHandler(results, nil);
+                return;
+            }
+            if (error == nil && data ==nil) {
+                NSError *error = [NSError errorWithDomain:@"Error: Image is nil" code:1 userInfo:nil];
+                completionHandler(nil, error);
+                return;
+            }
+            completionHandler(nil, error);
+        });
+    });
+}
+
+
 +(void)fetchImageInBackgroundFromURL:(NSURL *_Nonnull)url completionHandler:(kNSImageCompletionHandler)completionHandler{
     
     dispatch_queue_t imageQueue = dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_BACKGROUND, 0);
+//    dispatch_queue_t imageQueue = dispatch_queue_create(NULL, DISPATCH_QUEUE_SERIAL);
+
+//    DISPATCH_QUEUE_t imageQue = dispatch_queue_create();
     
     dispatch_async(imageQueue, ^{
         NSError *error;
